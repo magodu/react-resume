7aa65358bef4fb643970ce6188621e17ec3588f2
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-01-16 12:12:14 +0100

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import reactLogo from './assets/images/react_logo.png';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="title">
+                <h1>
+                    <img src={reactLogo} alt="" />
+                    My React resume project
+                </h1>
+                <h3>Typescript, SASS and GoogleMaps ready</h3>
+                <h3>First version</h3>
+            </div>
+        </div>
+    );
+}
+
+export default App;
