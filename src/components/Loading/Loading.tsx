328e8fed11faa2c263c001fba242bae4a105ec19
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-01-25 15:59:30 +0100

    change loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,27 @@
-import { useTranslation } from 'react-i18next';
-
 import classes from './Loading.module.scss';
-import loadingImage from '../../assets/images/loading.gif';
-
 
 const Loading = () => {
-    const [translate] = useTranslation('global');
 
     return (
-        <div className={classes.loading}><img src={loadingImage} alt={`{${translate('loading')}`} /></div>
+        <div className={classes.loading}>
+            <div className={classes.loader}>
+                <div className={classes.circle}>
+                    <div className={classes.inner}></div>
+                </div>
+                <div className={classes.circle}>
+                    <div className={classes.inner}></div>
+                </div>
+                <div className={classes.circle}>
+                    <div className={classes.inner}></div>
+                </div>
+                <div className={classes.circle}>
+                    <div className={classes.inner}></div>
+                </div>
+                <div className={classes.circle}>
+                    <div className={classes.inner}></div>
+                </div>
+            </div>
+        </div>
     );
 };
 
