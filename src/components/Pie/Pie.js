c881b32e7ccfeae643d34f39ce010094f02ccde4
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-01-27 19:12:47 +0100

    more funtionality to allow delay and number animation

--- src/components/Pie/Pie.js
+++ src/components/Pie/Pie.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './Pie.scss';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import classes from './Pie.module.scss';
 
 const cleanPercentage = (percentage) => {
     const tooLow = !Number.isFinite(+percentage) || percentage < 0;
@@ -26,10 +27,30 @@ const Circle = ({ colour, pct }) => {
 };
 
 const Text = ({ percentage, config }) => {
+
+    const [percentageText, setPercentageText] = useState('1');
+
+    const setValue = useCallback(() => {
+        let increment = 0;
+        const interval = setInterval(() => {
+            if (increment <= percentage) {
+                setPercentageText(increment.toFixed(0));
+                increment++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 15);
+
+    }, [percentage]);
+
+    useEffect(() => {
+        setValue();        
+    }, [setValue]);
+
     return (
         <React.Fragment>
             <text x="50%" y="50%" dominantBaseline="central" textAnchor="middle" fontSize={config.size || '2.5em'} fill={config.color || 'black'}>
-                {percentage.toFixed(0)}
+                {percentageText}
             </text>
             <text dx="10%" dy="4%" x="50%" y="50%" dominantBaseline="central" fontSize={"1.5em"}>%</text>
         </React.Fragment>
@@ -37,17 +58,30 @@ const Text = ({ percentage, config }) => {
 };
 
 const Pie = ({ percentage, title, config }) => {
-    const pct = cleanPercentage(percentage);
+    const [percentageBar, setPercentageBar] = useState(1);
+    const timer = config && config.delay ? parseInt(config.delay, 10) : 0;
+    
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            setPercentageBar(cleanPercentage(percentage));
+        }, timer);
+
+        return () => {
+            clearTimeout(identifier);
+        };
+    
+    }, [percentage, timer]);
+  
 
     return (
         <React.Fragment>
-            <div style={{textAlign: 'center'}}>
+            <div className={classes.pie}>
                 <svg width={config.width || 200} height={config.height || 200}>
                     <g transform={`rotate(270 ${'100 100'})`}>
                         <Circle colour={config.trackColor || 'lightgrey'} />
-                        <Circle colour={config.color} pct={pct} />
+                        <Circle colour={config.color} pct={percentageBar} />
                     </g>
-                    <Text percentage={pct} config={config.percentText || {}}/>
+                    <Text percentage={percentageBar} config={config.percentText || {}}/>
                 </svg>
             </div>
             <small>{title || ''}</small>
