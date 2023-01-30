163099e7b69db91d39749cc997fa05424f45cbec
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-01-30 11:40:05 +0100

    environment file and include it in gitignore file

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,13 +1,11 @@
 import React, { useMemo } from 'react';
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 
-const GOOGLE_MAPS_API_KEY = 'AIzaSyD-xSxeguqtWK4kNUrDTTU0gsNK0iXIYK4';   // TODO: move the key to environments
-
 const Map = ({ zoom, position }) => {
     const currentPosition = useMemo(() => position, [position]);
 
     return (
-        <GoogleMap zoom={zoom} center={currentPosition} disableDefaultUI="true" mapContainerClassName="map-container">
+        <GoogleMap zoom={zoom} center={currentPosition} mapContainerClassName="map-container">
             <MarkerF position={currentPosition}></MarkerF>
         </GoogleMap>
     );
@@ -15,7 +13,7 @@ const Map = ({ zoom, position }) => {
 
 const GoogleMapComponent = ({ zoom, position }) => {
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     });
 
     if (!isLoaded) return <div>Loading...</div>;
