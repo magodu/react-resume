ca67c0fd83ddd0b07bd8078a5e33bd43253dc677
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-02-13 14:35:03 +0100

    fix wrong literal

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,16 @@
 import { useTranslation } from 'react-i18next';
 
+import i18n from '../../i18n';
 import classes from './Loading.module.scss';
 import loadingImage from '../../assets/images/loading.gif';
 
 const Loading = () => {
-    const [translate] = useTranslation('global');
+    const [translate] = useTranslation('global', { i18n });
 
     return (
         <div className={classes.loading}>
-            <img src={loadingImage} alt={`{${translate('loading')}`} />
+            <img src={loadingImage} alt={`${translate('common.loading')}`} />
+            {/* <img src={loadingImage} alt="Cargando" /> */}
         </div>
     );
 };
